6233a8ea7b3eee76dcd9c3ac57824249cdc21ab2
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-17 17:47:48 +0530

    Added: Check attendance

---
+++ PresentOrAbsent.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class PresentOrAbsent {
+
+	public static void main(String[] args) {
+		Double isPresent=Math.random();
+		  if(isPresent>=0.5) {
+			  System.out.println("Employee is present");
+		  }
+		  else {
+			  System.out.println("Employee is absent");
+		  }
+	}
+
+	}
+
